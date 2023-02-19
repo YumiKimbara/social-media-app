717a4edf6726b9ef624f8bcb306f6424c0fb671a
Yumi <a13158y@gmail.com>
2023-02-19 15:26:37 -0800

    create postSchema of models

---
+++ server/controllers/posts.js
@@ -0,0 +1,5 @@
+// routesフォルダ内のroutesのlogicは全てcontrollersフォルダ内に記載する。
+
+export const getPosts = (req, res) => {
+  res.send("THIS WORKS!");
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express";
 
+// Reactでは語尾のjsを省略できるが、nodeでは省略しないように
+import { getPosts } from "../controllers/posts.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("THIS WORKS!");
-});
+router.get("/", getPosts);
 
 export default router;
