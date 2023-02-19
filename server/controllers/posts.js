d0be15420015f91279243f89ad85d8f44b8118da
Yumi <a13158y@gmail.com>
2023-02-19 15:54:02 -0800

    add try catch in the posts controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,28 @@
 // routesフォルダ内のroutesのlogicは全てcontrollersフォルダ内に記載する。
 
-export const getPosts = (req, res) => {
-  res.send("THIS WORKS!");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    // 非同期処理でpostをgetしたいのでasync awaitを使用する。
+    const postMessage = await PostMessage.find();
+    res.status(200).json(PostMessage);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    // 201はcreationが成功したということ。
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 // Reactでは語尾のjsを省略できるが、nodeでは省略しないように
-import { getPosts } from "../controllers/posts.js";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
