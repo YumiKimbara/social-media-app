7def48d63992d20c68d6565926a0c438035f97ce
Yumi <a13158y@gmail.com>
2023-02-22 19:00:19 -0800

    fix issue of like post

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,19 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes";
 
 export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload;
+    case LIKE:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
     case CREATE:
       // 全てのポストと新しいポストを一緒に返す。
       return [...posts, action.payload];
