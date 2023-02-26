07f6e39f1bc4d2c4bbfed0c5367799dfab92a7d1
Yumi <a13158y@gmail.com>
2023-02-26 12:33:59 -0800

    create Auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Container } from "@material-ui/core";
+import { BrouserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
   return (
-    <Container maxWidth="lg">
-      <Navbar />
-      <Home />
-    </Container>
+    <BrouserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrouserRouter>
   );
 };
 
