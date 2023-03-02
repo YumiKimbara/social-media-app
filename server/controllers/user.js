22c9edf78b9c429e13ce4b154e9c072391e6d85b
Yumi <a13158y@gmail.com>
2023-03-02 15:24:31 -0800

    create signup controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -12,7 +12,6 @@ export const signin = async (req, res) => {
     if (!existingUser)
       return res.status(404).json({ message: "User doesn't exist." });
 
-    // bcryptを使用しパスワードをハッシュ化するので、パスワードの安全性を高めることができる。
     const isPasswordCorrect = await bcrypt.compare(
       password,
       existingUser.password
@@ -30,6 +29,42 @@ export const signin = async (req, res) => {
     );
 
     res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const signup = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    if (password !== confirmPassword)
+      return res.status(400).json({ message: "Password doesn't match" });
+
+    // bcryptを使用しパスワードをハッシュ化するので、パスワードの安全性を高めることができる。
+    // 2つ目の引数は、ソルトといい、パスワードのハッシュ値を生成するために使用されるランダムなデータのことで、
+    // ハッシュの強度を増すために必要。2つ目の引数の値が大きいほど、ハッシュを計算するために必要な時間が長くなるが、ハッシュの強度も高くなる。
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await User.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
+
+    const token = jwt.sign(
+      { email: result.email, id: result._id },
+      // このtestという文字列はsecret key
+      "test",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result, token });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
