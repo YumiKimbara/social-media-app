bd527e3753911832647d7a28ee64820f04be7059
Yumi <a13158y@gmail.com>
2023-03-02 17:02:06 -0800

    create action auth

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,16 +1,24 @@
 import { AUTH } from "../constants/actionTypes";
 import * as api from "../api";
 
-export const signin = (form, navigate) => async () => {
+export const signin = (form, navigate) => async (dispatch) => {
   try {
+    const { data } = await api.signIn(form);
+
+    dispatch({ type: AUTH, data });
+
     navigate("/");
   } catch (error) {
     console.log(error);
   }
 };
 
-export const signup = (form, navigate) => async () => {
+export const signup = (form, navigate) => async (dispatch) => {
   try {
+    const { data } = await api.signUp(form);
+
+    dispatch({ type: AUTH, data });
+
     navigate("/");
   } catch (error) {
     console.log(error);
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,22 +1,25 @@
 import axios from "axios";
 
-const url = "http://localhost:3333/posts";
+const API = axios.create({ baseURL: "http://localhost:3333" });
 
 // urlに対してHTTP GETリクエストを送信し、APIから投稿データを取得する。
-export const fetchPosts = () => axios.get(url);
+export const fetchPosts = () => API.get("/posts");
 
-export const createPost = (newPost) => axios.post(url, newPost);
+export const createPost = (newPost) => API.post("/posts", newPost);
 
 // どの投稿をupdateしたいか判断するため、idも渡す。
 export const updatePost = (id, updatedPost) => {
   console.log("id, updatedPost", id, updatedPost);
-  axios.patch(`${url}/${id}`, updatedPost);
+  API.patch(`$/posts/${id}`, updatedPost);
 };
 
 export const deletePost = (id) => {
-  axios.delete(`${url}/${id}`);
+  API.delete(`posts/${id}`);
 };
 
 export const likePost = (id) => {
-  axios.patch(`${url}/${id}/likePost`);
+  API.patch(`posts/${id}/likePost`);
 };
+
+export const signIn = (formData) => API.post("/users/signin", formData);
+export const signUp = (formData) => API.post("/users/signup", formData);
